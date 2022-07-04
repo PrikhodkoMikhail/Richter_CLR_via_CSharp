710fadcb66a3aa6e4d7848d0581000918908174e
PrikhodkoMikhail <prixodkomv@gmail.com>
2022-07-04 14:31:33 +0300

    Check_code_compiler
    Chapter_1_page_53

---
+++ SomeLibrary/SomeLibrary/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+//Приказываем компилятору проверять код
+// на совместимость с CLS
+[assembly: CLSCompliant(true)]
+
+namespace SomeLibrary
+{
+    //Предупрждения выводяться, потому что класс является открытым 
+    sealed class SomeLibraryType
+    {
+        //Предупреждения: возвращаемый тип 'SomeLibrary.SomeLibraryType.Abc()'
+        //не является CLS-совместимым
+        public UInt32 Abc() { return 0; }
+
+        //Предупреждение: идентификаторы 'SomeLibrary.SomeLibraryType.abc()',
+        //отличается только регистром символов, не являются CLS-совместимыми
+        public void abc() { }
+
+        //Предупреждения нет: закрытый метод
+        private UInt32 ABC() { return 0; }
+    }
+}
